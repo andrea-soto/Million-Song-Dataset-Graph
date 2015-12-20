eadd6d5cb0bfbbd316658260d83308ef8bd93f42
Andrea Soto <andreasoto83@gmail.com>
2015-12-20 22:14:23 +0000

    Add msg with install path of Anaconda

--- config/install_anaconda.sh
+++ config/install_anaconda.sh
@@ -2,5 +2,10 @@
 
 cd /graph
 wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda2-2.4.1-Linux-x86_64.sh
+
+echo Ready to install Anaconda
+echo NOTE: When promted, please install Anaconda under '/graph/anaconda2/'
+echo 
+
 bash Anaconda2-2.4.1-Linux-x86_64.sh
 conda install h5py    
