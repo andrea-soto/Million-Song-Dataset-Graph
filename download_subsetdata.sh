373754755759174b4f8d4f06ac84c7e38932a508
Andrea Soto <andreasoto83@gmail.com>
2015-12-20 21:36:32 +0000

    Fix script that was overwritten by mistake

--- download_subsetdata.sh
+++ download_subsetdata.sh
@@ -1,30 +1,17 @@
 #!/usr/bin/env bash
 
-#Create a directory for the data
-mkdir data
-cd data
-
 # Download data subset of 10,000 songs, ~1GB to develop and test code
-wget http://static.echonest.com/millionsongsubset_full.tar.gz data_subset
+wget http://static.echonest.com/millionsongsubset_full.tar.gz
 wait
-
 tar xvzf millionsongsubset_full.tar.gz
 wait
+rm millionsongsubset_full.tar.gz
 
-# Download list of all artist ID 
-# The format is: artist id<SEP>artist mbid<SEP>track id<SEP>artist name
-wget http://labrosa.ee.columbia.edu/millionsong/sites/default/files/AdditionalFiles/unique_artists.txt
-wait
-wc -l unique_artists.txt #44745 unique_artists.txt
+cd MillionSongSubset
 
-# Download list of all unique artist terms (Echo Nest tags) 
-wget http://labrosa.ee.columbia.edu/millionsong/sites/default/files/AdditionalFiles/unique_terms.txt
-wait
-wc -l unique_terms.txt #7643 unique_terms.txt
-    
-# Download list of all unique artist musicbrainz tags
-wget http://labrosa.ee.columbia.edu/millionsong/sites/default/files/AdditionalFiles/unique_mbtags.txt
-wait
-wc -l unique_mbtags.txt #2321 unique_mbtags.txt
+# Download last-fm data with song similarities
+wget http://labrosa.ee.columbia.edu/millionsong/sites/default/files/lastfm/lastfm_subset.zip
+unzip lastfm_subset.zip
+rm lastfm_subset.zip
 
 cd ..
